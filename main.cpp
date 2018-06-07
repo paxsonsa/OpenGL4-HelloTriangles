970f9f6701d41a7bdec58ab1885c628eea85298f
Andrew Paxson <andrew@fx-mind.com>
2018-06-07 01:00:09 -0700

    hello triangle with working shaders

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
  * @Author: Andrew Paxson 
  * @Date: 2018-06-06 23:32:54 
  * @Last Modified by: Andrew Paxson
- * @Last Modified time: 2018-06-07 00:41:04
+ * @Last Modified time: 2018-06-07 00:59:10
  */
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
@@ -10,6 +10,37 @@
 
 int main(int argc, char const *argv[])
 {
+    GLFWwindow *window = nullptr;
+    const GLubyte* renderer;
+    const GLubyte* version;
+    GLuint vao, vbo;
+    GLfloat points[] = {
+        0.0f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f
+    };
+
+    /* these are the strings of code for the shaders
+	the vertex shader positions each vertex point */
+    const char *vertexShaderStr = "#version 410\n"
+        "in vec3 pos;"
+        "void main () {"
+        "   gl_Position = vec4 (pos, 1.0);"
+        "}";
+
+    // Fragment Shader
+    const char *fragmentShaderStr = "#version 410\n"
+        "out vec4 colour;"
+        "void main () {"
+        "   colour = vec4(0.5, 0.0, 0.5, 1.0);"
+        "}";
+
+    /* GL shader objects for vertex and fragment shader [components] */
+	GLuint vertexShader, fragmentShader;
+	/* GL shader programme object [combined, to link] */
+	GLuint shaderProgam;
+
+
     if (!glfwInit ()) {
         fprintf (stderr, "ERROR: could not start GLFW3\n");
         return 1;
@@ -21,7 +52,7 @@ int main(int argc, char const *argv[])
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello, Triangle!", nullptr, nullptr);
+    window = glfwCreateWindow(640, 480, "Hello, Triangle!", nullptr, nullptr);
     if (!window) {
         fprintf(stderr, "ERROR: Failed to Initialize the Window");
         glfwTerminate();
@@ -33,8 +64,8 @@ int main(int argc, char const *argv[])
     glewInit();
     
     // Version Info
-    const GLubyte* renderer = glGetString(GL_RENDERER);
-    const GLubyte* version = glGetString(GL_VERSION);
+    renderer = glGetString(GL_RENDERER);
+    version = glGetString(GL_VERSION);
     printf("Renderer: %s\n", renderer);
     printf("OpenGL Version: %s\n", version);
 
@@ -45,68 +76,41 @@ int main(int argc, char const *argv[])
     /*
         Work goes here
     */
-
-    // Vertex Data
-    GLfloat points[] = {
-        0.0f, 0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f
-    };
-    
     // Generate the Vertex Buffer Object and Store the Data
-    GLuint vbo = 0;
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
 
     // Generate the Vertex Array Object
-    GLuint vao = 0;
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-    // Our Shaders
-    // Vertex Shader
-    const char* vertexShaderStr =
-        "#version 410\n"
-        "in vec3 position;"
-        "void main () {"
-        "  gl_Position = vec4 (position, 1.0);"
-        "}";
-
-    // Fragment Shader
-    const char* fragmentShaderStr = 
-        "#version 410\n"
-        "out vec4 colour;\n"
-        "void main () {\n"
-        "   colour = vec4(0.5, 0.0, 0.5, 1.0);\n"
-        "}\n";
     
     // Create and Compile Shaders
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderStr, nullptr);
     glCompileShader(vertexShader);
-
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderStr, nullptr);
     glCompileShader(fragmentShader);
 
-    GLuint shaderProg = glCreateProgram();
-    glAttachShader(shaderProg, vertexShader);
-    glAttachShader(shaderProg, fragmentShader);
-    glLinkProgram(shaderProg);
+    shaderProgam = glCreateProgram();
+    glAttachShader(shaderProgam, fragmentShader);
+    glAttachShader(shaderProgam, vertexShader);
+    glLinkProgram(shaderProgam);
 
     
     while(!glfwWindowShouldClose(window)){
         // wipe the drawing buffer
-        glClearColor(0.6f, 0.6f, 0.8f, 1.0f);
-        // glUseProgram(shaderProg);
-        // glBindVertexArray(vao);
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        glUseProgram(shaderProgam);
+        glBindVertexArray(vao);
 
         // // Draw 0-3 points from the currently bound VAO with current in-use shader
-        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // Update other events like input handling
         glfwPollEvents();
